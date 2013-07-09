1184de15e4e54721477941ec4f7194584d20f6b8
Mark Doss <intern.mdoss@cpbgroup.com>
2013-07-09 16:49:19 -0400

    adding some javascript

--- list-javascript.js
+++ list-javascript.js
@@ -1,5 +1,19 @@
-function displayQuickview(e) {
+$(document).ready( function() {
+
+	$('#big-sur-button').hover(function(e){
+		// e.preventDefault();
+		$('#quickview').css('visibility', 'visible');
+
+		console.log("Clicked");	
+	}, function(e){
+		$('#quickview').css('visibility', 'hidden');
+
+	})
+})
+
+/*
+function displayQuickview() {
 	document.getElementById("quickview").style.visibility='visible';
-	e.stopPropagation();
 
 }
+*/
